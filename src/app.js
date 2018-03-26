8b0fccb984b18012c9e3f75e382b72993bcebcec
wenbobian3 <1198648157@qq.com>
2018-03-26 17:18:52 +0800

    第一次发布

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// import './static/js/layui.js'
+
+// import './static/js/nav.js'
+// import './static/js/index.js'
+
+// import './static/style/layui.css'
+// import './static/style/global.css'
+
+import App from './components/App'
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>欢迎来到德莱联盟</h1>
+      </div>
+    )
+  }
+}
+
+export default App
